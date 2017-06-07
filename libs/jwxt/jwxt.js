fac9c416bae68e713be57f02930242219df3272a
yuban12315 <fengyulin@imudges.com>
2017-06-07 14:29:32 +0800

    build

--- libs/jwxt/jwxt.js
+++ libs/jwxt/jwxt.js
@@ -1,4 +1,5 @@
-let request = require('superagent-charset')(require('superagent-retry')(require('superagent')))
+let request = require('superagent-charset')(require('superagent'))
+let cheerio=require('cheerio')
 let async=require('async')
 class jwxt {
     constructor(username, password) {
@@ -6,18 +7,55 @@ class jwxt {
         this.username = username
         this.password = password
         //内蒙古大学教务系统
-        this.baseUrl = 'http://jwxt.imu.edu.cn'
-        this.browserMsg={
-            "User-Agent":"Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36",
-            'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8'
+        this.baseUrl = 'http://202.207.0.238:8085'
+        this.browserMsg = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36",
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
         }
         //保存登录cookie
-        this.cookies={}
+        this.cookie = null
+    }
+    //登录
+    login(callback){
+        request.post(`${this.baseUrl}/loginAction.do`).redirects(0).charset('GBK').set(this.browserMsg).send({
+            zjh:this.getUsername(),
+            mm:this.getPassword()
+        }).end((err,res)=>{
+            if(!err){
+                if(res.hasOwnProperty('text')){
+                    console.log(res.text)
+                }
+                if(res.hasOwnProperty('header')){
+                    let cookie=res.header['set-cookie']
+                    if(cookie){
+                        this.setCookie(cookie)
+                        callback(null,'登录成功')
+                    }
+                }
+                else{
+                    callback(new Error('服务器错误',500))
+                }
+            }
+            else{
+                callback(err)
+            }
+        })
     }
 
-    //登录
-    login(){
-        request.post(`${this.baseUrl}/loginAction.do`)
+    getMainPage(callback){
+        let cookie=this.getCookie()
+        if(cookie!=null){
+            request.get(`${this.baseUrl}/xkAction.do`).set(this.browserMsg).set('Cookie',cookie)
+                .charset('GBK').end((err,res)=>{
+                if(res.hasOwnProperty('text')){
+                    callback(null,res.text)
+                }
+                else {
+                    callback(new Error('服务器错误'),500)
+                }
+            })
+        }
+        else callback(new Error('未登录'))
     }
 
     getUsername() {
@@ -26,6 +64,14 @@ class jwxt {
 
     getPassword() {
         return this.password
+    }
+
+    setCookie(cookie){
+        this.cookie=cookie
+    }
+
+    getCookie(){
+        return this.cookie
     }
 }
 module.exports=jwxt
--- libs/tests/jwxt_test.js
+++ libs/tests/jwxt_test.js
@@ -1,5 +1,27 @@
 let jwxt=require('./../jwxt/jwxt')
+let async=require('async')
 
 let test=new jwxt('0151123843','2535286679')
 
-console.log(test)
+async.waterfall([
+    (callback)=>{
+        test.login((err,res)=>{
+            if(err) callback(err)
+            else {
+                console.log(res)
+                callback(null)
+            }
+        })
+    },
+    (callback)=>{
+        test.getMainPage((err,res)=>{
+            if(err) callback(err)
+            else {
+                console.log(res)
+                callback(null)
+            }
+        })
+    }
+],(err,res)=>{
+    //集中处理err
+})
