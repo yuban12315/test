5ce298d453f1b1fa794103b8ecd74349481c2af4
yuban12315 <fengyulin@imudges.com>
2017-06-07 13:42:54 +0800

    build

--- libs/jwxt/jwxt.js
+++ libs/jwxt/jwxt.js
@@ -1,3 +1,31 @@
-class jwxt{
+let request = require('superagent-charset')(require('superagent-retry')(require('superagent')))
+let async=require('async')
+class jwxt {
+    constructor(username, password) {
+        //学号，登录密码
+        this.username = username
+        this.password = password
+        //内蒙古大学教务系统
+        this.baseUrl = 'http://jwxt.imu.edu.cn'
+        this.browserMsg={
+            "User-Agent":"Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36",
+            'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8'
+        }
+        //保存登录cookie
+        this.cookies={}
+    }
 
+    //登录
+    login(){
+        request.post(`${this.baseUrl}/loginAction.do`)
+    }
+
+    getUsername() {
+        return this.username
+    }
+
+    getPassword() {
+        return this.password
+    }
 }
+module.exports=jwxt
---
+++ libs/tests/jwxt_test.js
@@ -0,0 +1,5 @@
+let jwxt=require('./../jwxt/jwxt')
+
+let test=new jwxt('0151123843','2535286679')
+
+console.log(test)
